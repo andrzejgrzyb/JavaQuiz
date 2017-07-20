4aaa4c793a62f489d80225f7d4caa1e31f5f0974
Andrzej <mgr.inz.andrzej.grzyb@gmail.com>
2017-07-20 08:21:31 +0200

    code formatting

--- app/src/main/java/pl/com/andrzejgrzyb/javaquiz/MainActivity.java
+++ app/src/main/java/pl/com/andrzejgrzyb/javaquiz/MainActivity.java
@@ -13,13 +13,20 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.question1_radiogroup) RadioGroup question1RadioGroup;
-    @BindView(R.id.question2_checkBox1) CheckBox question2Checkbox1;
-    @BindView(R.id.question2_checkBox2) CheckBox question2Checkbox2;
-    @BindView(R.id.question2_checkBox3) CheckBox question2Checkbox3;
-    @BindView(R.id.question2_checkBox4) CheckBox question2Checkbox4;
-    @BindView(R.id.question3_radiogroup) RadioGroup question3RadioGroup;
-    @BindView(R.id.question4_edittext) EditText question4EditText;
+    @BindView(R.id.question1_radiogroup)
+    RadioGroup question1RadioGroup;
+    @BindView(R.id.question2_checkBox1)
+    CheckBox question2Checkbox1;
+    @BindView(R.id.question2_checkBox2)
+    CheckBox question2Checkbox2;
+    @BindView(R.id.question2_checkBox3)
+    CheckBox question2Checkbox3;
+    @BindView(R.id.question2_checkBox4)
+    CheckBox question2Checkbox4;
+    @BindView(R.id.question3_radiogroup)
+    RadioGroup question3RadioGroup;
+    @BindView(R.id.question4_edittext)
+    EditText question4EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,38 +41,33 @@ public class MainActivity extends AppCompatActivity {
         boolean missingAnswer = false;
         if (question1RadioGroup.getCheckedRadioButtonId() == -1) {
             missingAnswer = true;
-        }
-        else if (question1RadioGroup.getCheckedRadioButtonId() == R.id.question1_radioButton3) {
+        } else if (question1RadioGroup.getCheckedRadioButtonId() == R.id.question1_radioButton3) {
             score++;
         }
 
         if (!question2Checkbox1.isChecked() && !question2Checkbox2.isChecked() &&
                 !question2Checkbox3.isChecked() && !question2Checkbox4.isChecked()) {
             missingAnswer = true;
-        }
-        else if (question2Checkbox1.isChecked() && !question2Checkbox2.isChecked() &&
+        } else if (question2Checkbox1.isChecked() && !question2Checkbox2.isChecked() &&
                 question2Checkbox3.isChecked() && question2Checkbox4.isChecked()) {
             score++;
         }
 
         if (question3RadioGroup.getCheckedRadioButtonId() == -1) {
             missingAnswer = true;
-        }
-        else if (question3RadioGroup.getCheckedRadioButtonId() == R.id.question3_radioButton1) {
+        } else if (question3RadioGroup.getCheckedRadioButtonId() == R.id.question3_radioButton1) {
             score++;
         }
         final String userAnswer4 = question4EditText.getText().toString();
         if (userAnswer4.isEmpty()) {
             missingAnswer = true;
-        }
-        else if (userAnswer4.equalsIgnoreCase(getResources().getString(R.string.question4_answer))) {
+        } else if (userAnswer4.equalsIgnoreCase(getResources().getString(R.string.question4_answer))) {
             score++;
         }
 
         if (missingAnswer) {
             Toast.makeText(this, getResources().getString(R.string.not_answered), Toast.LENGTH_SHORT).show();
-        }
-        else {
+        } else {
             setScore(score);
         }
     }
